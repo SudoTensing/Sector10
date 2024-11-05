225e9e53e6d2389162060b0a227ec8c5534410c2
Tensing <admin@qnp.dk>
2024-11-05 17:45:39 +0100

    Add files via upload

---
+++ SoundLibrary.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundLibrary : MonoBehaviour
+{
+
+    [Header("sounds")]
+    public List<AudioClip> sounds = new List<AudioClip>();
+    public List<string> sound_names = new List<string>();
+    private Dictionary<string, AudioClip> lib = new Dictionary<string, AudioClip>();
+
+    void Start()
+    {
+        int index = 0;
+        foreach (AudioClip clip in sounds) {
+            lib.Add(sound_names[index], clip);
+            index++;
+        }
+
+        
+    }
+
+
+    public AudioClip returnAudio(string name) {
+        return lib[name];
+    }
+
+}
